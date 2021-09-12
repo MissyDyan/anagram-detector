295811a5761227811ce716a3443a2ade1b0a80b0
Diane Nguyen <diane.nguyen@gmail.com>
2021-09-12 16:51:00 -0700

    commit tree c1b7d6149f08b513477f158a109fb4d72eb55a93

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+    
+    attr_accessor :anagram
+
+    def initialize(word)
+        @anagram = word
+    end
+
+    def match(array)
+        array.find_all { |word| word.downcase.split("").sort == @anagram.downcase.split("").sort}
+    end
+
+end
